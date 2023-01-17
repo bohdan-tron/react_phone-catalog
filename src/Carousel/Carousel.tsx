0b02bc4278d4e678a16d54de1eb68671fd7b28b1
Bohdan Tron <ikwiczzts@gmail.com>
2023-01-17 16:24:56 +0200

    checkpoint 2

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -1,19 +1,78 @@
-import { useEffect, useState } from 'react';
+import {
+  useEffect,
+  useState,
+} from 'react';
+import classNames from 'classnames';
 import { sliderImgs } from '../Data/slider';
 
+import { ReactComponent as SvgChevronLeft } from '../img/ChevronLeft.svg';
+import { ReactComponent as SvgChevronRight } from '../img/ChevronRight.svg';
+
 export const Carousel: React.FC = () => {
-  const [translate, setTranslate] = useState(0);
+  const [translate, setTranslate] = useState({
+    pixels: 0,
+    sliderId: 1,
+  });
 
   const handleForward = () => {
-    return (translate > -2080)
-      ? setTranslate(translate - 1040)
-      : setTranslate(0);
+    return (translate.pixels > -2080)
+      ? setTranslate({
+        pixels: translate.pixels - 1040,
+        sliderId: translate.sliderId + 1,
+      })
+      : setTranslate({
+        pixels: 0,
+        sliderId: 1,
+      });
   };
 
   const handleBackward = () => {
-    return (translate < 0)
-      ? setTranslate(translate + 1040)
-      : setTranslate(-2080);
+    return (translate.pixels < 0)
+      ? setTranslate({
+        pixels: translate.pixels + 1040,
+        sliderId: translate.sliderId - 1,
+      })
+      : setTranslate({
+        pixels: -2080,
+        sliderId: 3,
+      });
+  };
+
+  const handleSliderChoose = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    id: number,
+  ) => {
+    const dataAttribute = event.currentTarget.getAttribute('data-px');
+
+    if (dataAttribute) {
+      setTranslate({
+        pixels: +(dataAttribute),
+        sliderId: id,
+      });
+    }
+  };
+
+  const pixelCalculation = (id: number) => {
+    let px = 0;
+
+    switch (id) {
+      case 1:
+        px = 0;
+        break;
+
+      case 2:
+        px = -1040;
+        break;
+
+      case 3:
+        px = -2080;
+        break;
+
+      default:
+        break;
+    }
+
+    return String(px);
   };
 
   useEffect(() => {
@@ -33,13 +92,17 @@ export const Carousel: React.FC = () => {
             type="button"
             aria-label="carousel backward button"
             onClick={handleBackward}
-          />
+          >
+            <SvgChevronLeft className="chevron" />
+          </button>
           <button
             className="carousel__button carousel__button--forward"
             type="button"
             aria-label="carousel forward button"
             onClick={handleForward}
-          />
+          >
+            <SvgChevronRight className="chevron" />
+          </button>
         </div>
 
         <div className="carousel__container">
@@ -48,7 +111,7 @@ export const Carousel: React.FC = () => {
               <li
                 key={slider.id}
                 className="carousel__item"
-                style={{ transform: `translate(${translate}px)` }}
+                style={{ transform: `translate(${translate.pixels}px)` }}
               >
                 <img
                   src={slider.path}
@@ -60,6 +123,27 @@ export const Carousel: React.FC = () => {
           </ul>
         </div>
 
+        <div className="carousel__pagination">
+          <ul className="carousel__indicators">
+            {sliderImgs.map(slider => (
+              <li
+                key={slider.id}
+                className="carousel__indicator"
+              >
+                <button
+                  type="button"
+                  aria-label="slider choose button"
+                  className={classNames(
+                    'carousel__rectangle',
+                    { active: translate.sliderId === slider.id },
+                  )}
+                  data-px={pixelCalculation(slider.id)}
+                  onClick={(e) => handleSliderChoose(e, slider.id)}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
