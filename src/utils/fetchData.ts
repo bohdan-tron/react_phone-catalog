ad06c2aec796bbb112b7a45a422e9fceeb84df84
bohdan-tron <ikwiczzts@gmail.com>
2023-01-18 17:32:03 +0200

    Checkpoint 3

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -29,30 +29,3 @@ export const getProductDetails = (
       return response.json();
     });
 };
-
-// function wait(delay: number) {
-//   return new Promise(resolve => {
-//     setTimeout(resolve, delay);
-//   });
-// }
-
-// function request<T>(
-//   url: string,
-//   method: RequestMethod = 'GET',
-// ): Promise<T> {
-//   const options: RequestInit = { method };
-
-//   return wait(300)
-//     .then(() => fetch(BASE_URL + url, options))
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error();
-//       }
-
-//       return response.json();
-//     });
-// }
-
-// export const client = {
-//   get: <T>(url: string) => request<T>(url),
-// };
