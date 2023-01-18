ab8850478b59b4fb52e8817dc451cd9546b1dfb0
bohdan-tron <ikwiczzts@gmail.com>
2023-01-18 19:05:50 +0200

    Checkpoint 3.1

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,21 @@ import { Header } from './Header/Header';
 
 import { ReactComponent as SvgChevronLeft } from './img/ChevronLeft.svg';
 import { ReactComponent as SvgChevronRight } from './img/ChevronRight.svg';
+import { ReactComponent as SvgFavsUnfilled } from './img/FavsUnfilled.svg';
+// import { getProducts } from './utils/fetchData';
 
 const App = () => {
+  // const fetchProducts = async () => {
+  //   try {
+  //     const resp = await getProducts();
+  //     console.log(resp);
+  //   } catch (error) {
+  //     throw new Error();
+  //   }
+  // };
+
+  // fetchProducts();
+
   return (
     <div className="App">
 
@@ -32,6 +45,53 @@ const App = () => {
             >
               <SvgChevronRight className="chevron" />
             </button>
+
+            <div className="prodslider">
+              <ul className="prodslider__list">
+                <li className="prodslider__item">
+                  <div className="card">
+                    <img
+                      src="img/phones/motorola-atrix-4g.0.jpg"
+                      alt="product"
+                      className="card__image"
+                    />
+                    <p className="card__name">
+                      Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
+                    </p>
+                    <h2 className="card__discount-price">$799</h2>
+                    <p className="card__price">$899</p>
+                    <hr />
+                    <p className="card__screen">
+                      Screen
+                      <span className="card__screen-spec">5.8” OLED</span>
+                    </p>
+                    <p className="card__capacity">
+                      Capacity
+                      <span className="card__capacity-spec">64 GB</span>
+                    </p>
+                    <p className="card__ram">
+                      RAM
+                      <span className="card__ram-spec">4 GB</span>
+                    </p>
+
+                    <div className="card__buttons">
+                      <button
+                        type="button"
+                        className="card__add"
+                      >
+                        Add to cart
+                      </button>
+                      <button
+                        type="button"
+                        className="card__fav"
+                      >
+                        <SvgFavsUnfilled />
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
